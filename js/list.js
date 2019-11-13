0b1d77c76794d9bb826d0e6ca2affb194b306625
Gibstock <gibstock@gmail.com>
2019-11-13 12:46:12 -0800

    fixed list names

--- js/list.js
+++ js/list.js
@@ -3,9 +3,9 @@ export {recipeList};
 
 let recipeList = [
     'calabasa-with-rice',
-    'taco cups', 
-    'apple pie cookies', 
-    'cheesy chicken muffin pizzas', 
-    'slow cooker herbed chicken and potatoes', 
+    'taco-cups', 
+    'apple-pie-cookies', 
+    'cheesy-chicken-muffin-pizzas', 
+    'slow-cooker-herbed-chicken-and-potatoes', 
     'pancake bites'
 ];
